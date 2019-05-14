9bc8f1fb0600603c37508ed6fc18fb9a6905fdfd
GeniusK <geniusk1047@naver.com>
2019-05-14 14:52:45 +0900

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  redirectUri: 'http://192.168.31.128/authorize',
-  clientId: 'CIDPL49TEFPSPLZS',
-  clientSecret: 'P7WV8A8PKWLB5YY35QNQP6WPH219APOK',
-  appId: 'vTL0qHdlXXXp'
-}
