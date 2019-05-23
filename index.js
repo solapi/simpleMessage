a2ba7a9d40b86b1ed39b9ffa660ee9eb2d9a1c06
hosy <hosy@nurigo.net>
2019-05-23 15:29:32 +0900

    index.js 수정

--- index.js
+++ index.js
@@ -16,18 +16,10 @@ app.set('views', __dirname + '/public/views')
 app.set('view engine', 'ejs')
 app.engine('html', require('ejs').renderFile)
 
-// 첫 페이지 (인증 정보들 입력) view
+// 첫 페이지 View
 app.get('/', (req, res) => {
   const { APP_COOKIE } = req.cookies
-  const info = {
-    state: nanoId(),
-    appId: '',
-    clientId: '',
-    clientSecret: '',
-    redirectUri: '',
-    scope: 'message:write'
-  }
-  if (APP_COOKIE) Object.assign(info, APP_COOKIE)
+  const info = { state: nanoId(), redirectUri, scope: 'message:write' }
   return res.render('index', info)
 })
 
@@ -35,46 +27,24 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => res.render('login'))
 
 // 문자 전송 view
-app.get('/send', (req, res) =>
-  res.render('send', { result: req.query.result, type: req.query.type })
-)
-
-// 설정 저장하고 다시 초기 화면으로 redirect
-app.post('/config', async (req, res) => {
-  return res
-    .cookie('APP_COOKIE', req.body, {
-      httpOnly: false,
-      signed: false,
-      encode: String
-    })
-    .redirect('/')
+app.get('/send', (req, res) => {
+  const { result, type } = req.query
+  return res.render('send', { result, type })
 })
 
 // 앱 관련 정보 불러와서 authorize로 redirect 시켜줌
 app.get('/auth', (req, res) => {
   const { state, scope } = req.query
-  const { clientId, redirectUri } = getAuthInfo(req.cookies)
   return res.redirect(
     `${host}/oauth2/v1/authorize?client_id=${clientId}&state=${state}&scope=${scope}&response_type=code&redirect_uri=${redirectUri}`
   )
 })
 
-// 앱 관련 정보 불러오는 함수
-// 쿠키에 있으면 쿠키의 정보를, 없으면 config의 정보를 RETURN 함
-function getAuthInfo(cookies) {
-  const { APP_COOKIE } = cookies
-  const info = { clientId, clientSecret, redirectUri, appId }
-  if (APP_COOKIE && APP_COOKIE.clientId) {
-    Object.assign(info, APP_COOKIE)
-  }
-  return info
-}
-
-// 인증 처리 API
+// 넘겨받은 authorizationCode 값으로 accessToken값을 발급받아,
+// Solapi API를 사용하기위해 Cookie에 값을 넣어주는 API
 app.get('/authorize', async (req, res) => {
   try {
     const { code } = req.query
-    const { clientId, clientSecret, redirectUri } = getAuthInfo(req.cookies)
     const { access_token } = await request({
       method: 'POST',
       uri: `${host}/oauth2/v1/access_token`,
@@ -87,13 +57,11 @@ app.get('/authorize', async (req, res) => {
       },
       json: true
     })
-    return res
-      .cookie('APP_COOKIE', access_token, {
-        httpOnly: false,
-        signed: false,
-        encode: String
-      })
-      .redirect('/send')
+    return res.cookie('APP_COOKIE', access_token, {
+      httpOnly: false,
+      signed: false,
+      encode: String
+    }).redirect('/send')
   } catch (err) {
     const { errorCode, errorMessage } = err.error
     return res.redirect(`send?result=${errorCode}-${errorMessage}`)
@@ -106,14 +74,12 @@ app.post('/send', async (req, res) => {
     body: { text, to, from },
     cookies: { APP_COOKIE }
   } = req
-  const { appId } = getAuthInfo(req.cookies)
+
   try {
     const result = await request({
       method: 'POST',
       uri: `${host}/messages/v4/send`,
-      headers: {
-        Authorization: `bearer ${APP_COOKIE}`
-      },
+      headers: { Authorization: `bearer ${APP_COOKIE}` },
       body: {
         message: { text, to, from },
         agent: { appId }
